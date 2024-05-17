2bdb2d9e59c08c9aa218aed42b797cf650c22ece
ArturLib <arturliber@gmail.com>
2024-05-17 13:31:57 +0300

    lesson_2 - homework is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
 import './App.css';
 
-import React from 'react';
+import React, {useState} from 'react';
+
+interface IState {
+    value: number
+}
 
 const App = () => {
-  return (
+
+    const [counter, setCounter] = useState<IState>({value: 0})
+
+    const increment = () => {
+        // ПЕРШИЙ ВАРІАНТ (не по важливості):
+        // setCounter({value: counter.value + 1});
+            // Тут ми значення перепризначаємо {value: counter.value + 1}.
+            // {value: counter.value + 1} - такий запис тому, що в нас параметризація через інтерфейс IState
+
+        // ДРУГИЙ ВАРІАНТ (не по важливості):
+        // Якщо ми працюємо з примітивними типами, то
+        // counter.value = 100500;
+            // тут ми в лоб визначили чому дорівнює значення
+        // setCounter({...counter})
+            // обовʼязково потрібна деструктуризація counter для того, щоб setCounter міг
+            // працювати, по іншому він буде ТІЛЬКИ присвоювати 100500 для counter
+
+        // ТРЕТІЙ ВАРІАНТ (не по важливості):
+        setCounter(prevState => ({value: prevState.value + 1}))
+
+    // ПІДСУМОК: Якщо потрібно впровадити значення на щось конкретне, то використовуємо setCounter і присвоюємо конкретне
+    // значення. Якщо значення потрібно постійно оновлювати, то найкраще передавати через кол-бек ф-цію (3 варіант)
+    };
+
+    const decrement = () => {
+        setCounter(prevState => ({value: prevState.value - 1}))
+    };
+
+    const reset = () => {
+        setCounter({value: 0})
+    };
+
+    return (
       <div>
-        App
+          <h2>{counter.value}</h2>
+          <button onClick={increment}>increment</button>
+          <button onClick={decrement}>decrement</button>
+          <button onClick={reset}>reset</button>
       </div>
   );
 };
