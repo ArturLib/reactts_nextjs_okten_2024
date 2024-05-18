26b6f20471a873c7f3c8614407f11f4663e2e040
ArturLib <arturliber@gmail.com>
2024-05-18 19:04:30 +0300

    lesson_3 - hooks useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css';
 
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
 const App = () => {
-  return (
+
+    const [x, setX] = useState<number>(0);
+
+    const y = useRef(0);
+
+    console.log('work');
+
+    return (
       <div>
-        App
+
+          <button onClick={() => {setX((x) => x + 1)}}>change x {x}</button>
+          <button onClick={() => {
+              y.current++;
+              console.log(y);
+          }}>change y {y.current}</button>
+
       </div>
   );
 };
