3f5089555631f6bb2c214d02c51988b754df6d09
ArturLib <arturliber@gmail.com>
2024-06-11 18:03:32 +0300

    lesson_12 - state manager, zustand

--- src/context/ContextProvider.ts
+++ src/context/ContextProvider.ts
@@ -1,10 +1,13 @@
 import {createContext, useContext} from "react";
 import {IUserModel} from "../models/IUserModel";
 import {IPostModel} from "../models/IPostModel";
+import {create} from "zustand";
 
 type StoreType = {
     userStore: {
         allUsers: IUserModel[],
+        loadUsers: (users: IUserModel[]) => void,
+        favoriteUser: IUserModel | null,
         setFavoriteUser: (obj: IUserModel) => void
     },
     postStore: {
@@ -12,16 +15,37 @@ type StoreType = {
     }
 };
 
-export const defaultValue: StoreType = {
-    userStore: {
-        allUsers: [],
-        setFavoriteUser: () => {}
-    },
-    postStore: {
-        allPosts: []
+export const useStore = create<StoreType>()((set) => {
+    return {
+        userStore: {
+            allUsers: [],
+            loadUsers: (users: IUserModel[]) => {
+                return set((state: StoreType) => {
+                    return {
+                        ...state,
+                        userStore: {
+                            ...state.userStore,
+                            allUsers: users
+                        }
+                    }
+                })
+            },
+            favoriteUser: null,
+            setFavoriteUser: (user: IUserModel) => {
+                return set(state => {
+                    return {
+                        ...state,
+                        userStore: {
+                            ...state.userStore,
+                            favoriteUser: user
+                        }
+                    }
+                })
+            }
+        },
+        postStore: {
+            allPosts: []
+        }
     }
-};
+})
 
-export const Context = createContext<StoreType>(defaultValue);
-
-export const useContextProvider = (): StoreType => useContext(Context);
